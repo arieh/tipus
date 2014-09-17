5973ac79f5c4f7ab488698a3161683832bbd3171
Arieh Glazer <arieh@chegg.com>
2014-09-17 22:23:52 +0300

    better minified code

--- minify.js
+++ minify.js
@@ -1,21 +1,33 @@
 var UglifyJS = require("uglify-js");
 var fs = require('fs');
+var core_list = [
+        "parse/public/bower_components/jquery/dist/jquery.js",
+        "parse/public/bower_components/underscore/underscore.js",
+        "parse/public/bower_components/backbone/backbone.js",
+        "parse/public/bootstrap/js/bootstrap.js"
+    ],
+    main_list = [
+        "parse/public/js/Data.js",
+        "parse/public/js/Names.js",
+        "parse/public/js/Page.js",
+        "parse/public/js/Stage.js",
+        "parse/public/js/Views.js",
+        "parse/public/js/Category.js",
+        "parse/public/js/Climber.js",
+        "parse/public/js/Nav.js",
+        "parse/public/js/main.js"
+    ];
 
-var list =[
-    "parse/public/js/Data.js",
-    "parse/public/js/Names.js",
-    "parse/public/js/Page.js",
-    "parse/public/js/Stage.js",
-    "parse/public/js/Views.js",
-    "parse/public/js/Category.js",
-    "parse/public/js/Climber.js",
-    "parse/public/js/Nav.js",
-    "parse/public/js/main.js"
-]
+function minify(list, name, next){
+    var results = UglifyJS.minify(list);
+    console.log("Writing " + name + " file");
+    fs.open('parse/public/js/'+name+'.js', 'a+', function(){
+        fs.writeFile('parse/public/js/'+name+'.js',results.code);
+        next && next();
+    });
 
-var results = UglifyJS.minify(list);
+}
 
-console.log("Writing Minified file");
-fs.open('parse/public/js/min.js', 'a+', function(){
-    fs.writeFile('parse/public/js/min.js',results.code);
+minify(core_list,'min.core', function(){
+    minify(main_list,'min');
 });
